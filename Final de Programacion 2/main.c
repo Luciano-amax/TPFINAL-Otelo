d1b75119156f6c853c69e3c0ff0bb04a5579a8ae
Luciano-amax <82392830+Luciano-amax@users.noreply.github.com>
2022-12-25 15:23:26 -0300

    Inicio de proyecto
    Archivos de c y python

---
+++ Final de Programacion 2/main.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+
+// hola mundo;
+
+
+void Pedir_numero() {
+    int number;
+    printf("Ingrese un numero por favor: ");
+    scanf("%d",&number);
+    printf("Ingresaste el numero: %d",number);
+
+}
+
+
+ int main(){
+    
+    Pedir_numero();
+    return 0;
+ }
